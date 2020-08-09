2353a8a99deb9d999cd6ebb9bccb1074d952c281
chirag <chiragkaushik517@gmail.com>
2020-08-09 16:22:26 +0530

    Add Models and registered these models with admin interface

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from testapp.models import BaseModel,TodoList, Access, TodoItem
 # Register your models here.
+
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'created_at', 'updated_at', 'is_active']
+
+class AccessAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_id', 'list_id', 'permission_type', 'created_at', 'updated_at', 'is_active' ]
+
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'list_id', 'heading', 'scheduled_on', 'created_at', 'updated_at', 'is_active']
+
+admin.site.register(TodoList, TodoListAdmin)
+admin.site.register(TodoItem, TodoItemAdmin)
+admin.site.register(Access, AccessAdmin)
--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class TodoList(BaseModel):
+    name = models.CharField(max_length=30)
+
+    # created_by = models.ForeignKey(user, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Access(BaseModel):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    list_id = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    permission_type = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class TodoItem(BaseModel):
+    list_id = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    heading = models.CharField(max_length=30)
+    scheduled_on = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.heading
+
+
+
